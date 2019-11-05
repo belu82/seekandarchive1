c9f19a830cf499e4a0b2c3bc453e78ec5288e5b1
belu82 <44766864+belu82@users.noreply.github.com>
2019-11-05 13:33:18 +0100

    Add project files.

---
+++ SeekAndArchive/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.IO.Compression;
+
+namespace SeekAndArchive
+{
+    class Program
+    {
+        static List<FileInfo> FoundFiles;
+        static List<FileSystemWatcher> watchers;
+        static List<DirectoryInfo> archiveDirs;
+        static void Main(string[] args)
+        {
+
+            string fileName = args[0];
+            string directoryName = args[1];
+
+            archiveDirs = new List<DirectoryInfo>();
+            FoundFiles = new List<FileInfo>();
+            watchers = new List<FileSystemWatcher>();
+
+            DirectoryInfo rootDir = new DirectoryInfo(directoryName);
+
+            if (!rootDir.Exists)
+            {
+                Console.WriteLine("The specified directory does not exist!");
+                return;
+            }
+
+            RecursiveSearch(FoundFiles, fileName, rootDir);
+            Console.WriteLine("Found {0} files.", FoundFiles.Count);
+
+
+            for (int i =0;i<FoundFiles.Count; i++)
+            {
+                archiveDirs.Add(Directory.CreateDirectory("archive" + i.ToString()));
+            }
+
+            foreach(FileInfo fil in FoundFiles)
+            {
+                Console.WriteLine("{0}", fil.FullName);
+
+            }
+
+            foreach (FileInfo fil in FoundFiles)
+            {
+                FileSystemWatcher newWatcher = new FileSystemWatcher(fil.DirectoryName, fil.Name);
+                newWatcher.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite;
+                newWatcher.Changed += new FileSystemEventHandler(WatcherChanged);
+                newWatcher.EnableRaisingEvents = true;
+                watchers.Add(newWatcher);
+            }
+            
+            Console.ReadKey();
+        }
+
+        static void RecursiveSearch(List<FileInfo> foundFiles, string fileName, DirectoryInfo currentDirectory)
+        {
+            foreach(FileInfo fil in currentDirectory.GetFiles())
+            {
+                if(fil.Name == fileName)
+                {
+                    foundFiles.Add(fil);
+                }
+            }
+            foreach(DirectoryInfo dir in currentDirectory.GetDirectories())
+            {
+                RecursiveSearch(foundFiles, fileName, dir);
+            }
+
+        }
+        static void ArchiveFile(DirectoryInfo archiveDir, FileInfo fileToArchive)
+        {
+            FileStream input = fileToArchive.OpenRead();
+            FileStream output = File.Create(archiveDir.FullName + @"" + fileToArchive.Name + ".gz");
+            GZipStream Compressor = new GZipStream(output, CompressionMode.Compress);
+                
+            int b = input.ReadByte();
+
+            while(b != -1)
+            {
+                Compressor.WriteByte((byte)b);
+
+                b = input.ReadByte();
+            }
+            Compressor.Close();
+            input.Close();
+            output.Close();
+        }
+
+        static void WatcherChanged(object sender, FileSystemEventArgs e)
+        {
+
+            if (e.ChangeType == WatcherChangeTypes.Changed)
+            {
+                Console.WriteLine("{0} has been changes", e.FullPath);
+                FileSystemWatcher senderWatcher = (FileSystemWatcher)sender;
+                int index = watchers.IndexOf(senderWatcher, 0);
+             
+                //now that we have the index, we can archive the file
+                ArchiveFile(archiveDirs[index], FoundFiles[index]);
+            }
+
+            
+
+        }
+
+                          
+    }
+
+         
+}
